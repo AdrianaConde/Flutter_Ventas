e67711b32e46aa6390c563250e875d1216de0452
AdrianaConde <adriana.conde@unl.edu.ec>
2019-08-14 13:04:08 -0500

    VistaCliente

--- lib/menu/Cliente.dart
+++ lib/menu/Cliente.dart
@@ -19,6 +19,7 @@ class _ClientePageState extends State<ClientePage> {
                 padding: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
                 child: Column(
                   children: <Widget>[
+
                     //---------Nombre y Apellido-----------------
                     TextField(
                       decoration: InputDecoration(
@@ -29,38 +30,11 @@ class _ClientePageState extends State<ClientePage> {
                             borderSide: BorderSide(color: Colors.green)),
                       ),
                     ),
-                    //--------------------------
-                    SizedBox(height: 20.0),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'DNI',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
-                    ),
-                    SizedBox(height: 5.0),
-                    //--------------------------
-                    SizedBox(height: 20.0),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: '',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
-                    ),
-                    SizedBox(height: 5.0),
 
                     //--------------------------
-                    SizedBox(height: 20.0),
                     TextField(
                       decoration: InputDecoration(
-                          labelText: '',
+                          labelText: 'Teléfono',
                           labelStyle: TextStyle(
                               fontFamily: 'Montserrat',
                               fontWeight: FontWeight.bold,
@@ -68,18 +42,46 @@ class _ClientePageState extends State<ClientePage> {
                           focusedBorder: UnderlineInputBorder(
                               borderSide: BorderSide(color: Colors.green))),
                     ),
-                    SizedBox(height: 5.0),
+                    
+                    //--------------------------
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Cédula',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green)
+                        )),
+                    ),
+                    
+                    //--------------------------
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Referencias',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green)
+                      )),
+                    ),
+                    
+                    RaisedButton(
+                    disabledColor: Colors.amber,
+                    child: Text("Guardar"),
+                    splashColor: Colors.amber,
+                    color: Colors.blueAccent,
+                    onPressed: ()  {
+                      print("Hola Raised Button");
+                    },
+                    ),
                   ],
                 ),
               ),
-          ]), floatingActionButton: FloatingActionButton(
-            tooltip: 'Registrar',
-            child: Material(
-
-            ), onPressed: (){
-              
-            },
-          ),
-          );
+          ]), 
+    );
   }
 }
